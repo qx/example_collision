55cb8d233980be4e2e0563fb42b89f1cb3bf8413
ouyangzk1 <ouyangzk1@lenovo.com>
2014-08-18 16:15:08 +0800

    碰撞检测

--- js/assetfactory.js
+++ js/assetfactory.js
@@ -9,28 +9,28 @@
     this.assetsLoaded = 0;
   }
 
-  p.loadAssets = function(array) {
-    for (var c = 0; c < array.length; c++) {
-      this.loadAsset(array[c]);
-    }
-  }
+    p.loadAssets = function (array) {
+        for (var c = 0; c < array.length; c++) {
+            this.loadAsset(array[c]);
+        }
+    };
 
-  p.loadAsset = function(url) {
-    var image = new Image();
-    this[url] = image;
-    this.assetsToLoad++;
-    image.onload = this.onImageLoaded.bind(this);
+    p.loadAsset = function (url) {
+        var image = new Image();
+        this[url] = image;
+        this.assetsToLoad++;
+        image.onload = this.onImageLoaded.bind(this);
 
-    image.src = url;
-  }
+        image.src = url;
+    };
 
-  p.onImageLoaded = function(e) { 
-    this.assetsLoaded++;
+    p.onImageLoaded = function (e) {
+        this.assetsLoaded++;
 
-    if ( this.assetsLoaded == this.assetsToLoad ) {
-      if ( this.onComplete ) this.onComplete();
-    }
-  }
+        if (this.assetsLoaded == this.assetsToLoad) {
+            if (this.onComplete) this.onComplete();
+        }
+    };
 
   scope.AssetFactory = AssetFactory; 
 } (window));
